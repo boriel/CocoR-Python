e1ca724fdfe41a72d3486a7fc41cec7f6381fd5d
Jose Rodriguez <boriel@gmail.com>
2020-02-06 11:41:59 +0100

    Add open_file() function in Generator

--- Coco/generator.py
+++ Coco/generator.py
@@ -3,5 +3,37 @@
 
 __all__ = ['Generator']
 
+import os
+from typing import BinaryIO
+
+from .tab import Tab
+from .errors import FatalError
+
+
+# -----------------------------------------------------------------------------
+#  Generator
+# -----------------------------------------------------------------------------
 class Generator:
-    pass
+    EOF = -1
+    tab: Tab
+    frame_file: str
+    fram: BinaryIO
+
+    def __init__(self, tab: Tab):
+        self.tab = tab
+
+    def open_file(self, fname) -> BinaryIO:
+        if self.tab.frameDir is not None:
+            self.frame_file = os.path.join(self.tab.frameDir, fname)
+            if not os.path.exists(self.frame_file):
+                raise FatalError("Cannot find file {}".format(self.frame_file))
+
+            if not os.path.isfile(self.frame_file):
+                raise FatalError("'{}' is not a regular file".format(self.frame_file))
+
+        try:
+            self.fram = open(self.frame_file, 'rb')
+        except OSError:
+            raise FatalError("Cannot open file: {}".format(self.frame_file))
+
+        return self.fram
