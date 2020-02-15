5216e8a9d4f55ee2d9a4586fa6b8cde114c9ccc3
Jose Rodriguez <boriel@gmail.com>
2020-02-15 11:18:03 +0100

    Added Coco() method to parser
    Also other minor methods implemented.

--- Coco/parser.py
+++ Coco/parser.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import sys
 
 from .errors import Errors
 from .scanner import Scanner, Token
 from .trace import Trace
-from .tab import Tab, Position
+from .tab import Tab, Position, Node, Graph, Symbol
 from .dfa import DFA
 from .charset import CharSet
 from .parsergen import ParserGen
@@ -133,6 +134,159 @@ class Parser:
             self.get()
             self.dfa.ignore_case = True
 
+        if self.la.kind == 8:
+            self.get()
+            while self.la.kind == 1:
+                self.set_decl()
+
+        if self.la.kind == 9:
+            self.get()
+            while self.la.kind in (1, 3, 5):
+                self.token_decl(Node.t)
+
+        if self.la.kind == 10:
+            self.get()
+            while self.la.kind in (1, 3, 5):
+                self.token_decl(Node.pr)
+
+        while self.la.kind == 11:
+            self.get()
+            nested: bool = False
+            self.expect(12)
+            g1 = self.token_expr()
+            self.expect(13)
+            g2 = self.token_expr()
+            if self.la.kind == 14:
+                self.get()
+                nested = True
+
+            self.dfa.new_comment(g1.l, g2.l, nested)
+
+        while self.la.kind == 15:
+            self.get()
+            s = self.set()
+            self.tab.ignored.or_(s)
+
+        while self.la.kind not in (0, 16):
+            self.syn_err(45)
+            self.get()
+
+        self.expect(16)
+        if self.genScanner:
+            self.dfa.make_deterministic()
+        self.tab.delete_nodes()
+
+        while self.la.kind == 1:
+            self.get()
+            sym: Symbol = self.tab.find_sym(self.t.val)
+            undef: bool = sym is None
+            if undef:
+                sym = self.tab.new_sym(Node.nt, self.t.val, self.t.line)
+            else:
+                if sym.typ == Node.nt:
+                    if sym.graph is not None:
+                        self.sem_err("'{}' name declared twice".format(sym.name))
+                else:
+                    self.sem_err("this symbol kind not allowed on left side of production")
+                sym.line = self.t.line
+
+            no_attrs: bool = sym.attrPos is None
+            sym.attrPos = None
+            no_ret: bool = sym.retVar is None
+            sym.retVar = None
+
+            if self.la.kind in (24, 29):
+                self.attr_decl(sym)
+
+            if not undef:
+                if no_attrs != (sym.attrPos is None) or no_ret != (sym.retVar is None):
+                    self.sem_err("attribute mismatch between declaration and use of this symbol")
+
+            if self.la.kind == 42:
+                sym.semPos = self.sem_text()
+
+            self.expect_weak(17, 3)
+            g = self.expression()
+            sym.graph = g.l
+            self.tab.finish(g)
+            self.expect_weak(18, 4)
+
+        self.expect(19)
+        self.expect(1)
+        if gram_name != self.t.val:
+            self.sem_err("'{}' name does not match grammar name".format(self.t.val))
+
+        self.tab.gramSy = self.tab.find_sym(gram_name)
+        if self.tab.gramSy is None:
+            self.sem_err("missing production for grammar name")
+        else:
+            sym = self.tab.gramSy
+            if sym.attrPos is not None:
+                self.sem_err("grammar symbol must not have attributes")
+
+        self.tab.noSym = self.tab.new_sym(Node.t, "???", 0)  # noSym gets highest number
+        self.tab.setup_anys()
+        self.tab.renumber_pragmas()
+
+        if self.tab.ddt[2]:
+            self.tab.print_nodes()
+
+        if self.errors.count == 0:
+            sys.stdout.write("checking\n")
+            self.tab.comp_symbol_sets()
+            if self.tab.ddt[7]:
+                self.tab.XRef()
+                if self.tab.grammar_ok():
+                    sys.stdout.write("parser")
+                    self.pgen.write_parser()
+
+                    if self.genScanner:
+                        sys.stdout.write(" + scanner")
+                        self.dfa.write_scanner()
+                        if self.tab.ddt[0]:
+                            self.dfa.print_states()
+
+                    sys.stdout.write(" generated\n")
+                    if self.tab.ddt[8]:
+                        self.pgen.write_statistics()
+
+                if self.tab.ddt[6]:
+                    self.tab.print_symbol_table()
+                self.expect(18)
+
+    def set_decl(self):
+        self.expect(1)
+        name = self.t.val
+        c = self.tab.find_CharClass(name)
+        if c is not None:
+            self.sem_err("'{}' name declared twice".format(name))
+
+        self.expect(17)
+        s = self.set()
+        if s.elements() == 0:
+            self.sem_err("character set must not be empty")
+
+        self.tab.new_CharClass(name, s)
+        self.expect(18)
+
+    def token_decl(self, typ: int):
+        pass
+
+    def token_expr(self) -> Graph:
+        g: Graph = self.token_term()
+        first: bool = True
+
+        while self.weak_separator(33, 7, 8):
+            g2: Graph = self.token_term()
+            if first:
+                self.tab.make_first_alt(g)
+                first = False
+            self.tab.make_alternative(g, g2)
+
+        return g
+
+    def set(self) -> CharSet:
+        pass
 
 
     set_ = [
